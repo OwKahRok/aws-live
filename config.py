fdfa8b49fe77456b6245c7abb74d58cccf778ec8
OwKahRok <162780798+OwKahRok@users.noreply.github.com>
2024-04-16 16:04:29 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "database-2.clysog0kwjbw.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Aacs3553"
 customdb = "Employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "owkahrok-employee"
 customregion = "us-east-1"
 
